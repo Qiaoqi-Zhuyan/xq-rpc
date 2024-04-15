9ad0fc7493a29c756632ae613966479ba60e7da4
Qiaoqi-Zhuyan <shuxy6263@163.com>
2024-04-15 23:23:38 +0800

    增加hessian序列化器

---
+++ xq-rpc-core/src/main/java/org/xq/xqrpc/serializer/HessianSerializer/HessianSerializer.java
@@ -0,0 +1,29 @@
+package org.xq.xqrpc.serializer.HessianSerializer;
+
+import com.caucho.hessian.io.HessianInput;
+import com.caucho.hessian.io.HessianOutput;
+import org.xq.xqrpc.serializer.Serializer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * Hessian 序列化器
+ */
+public class HessianSerializer implements Serializer {
+    @Override
+    public <T> byte[] serialize(T object) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        HessianOutput ho = new HessianOutput(bos);
+        ho.writeObject(object);
+        return bos.toByteArray();
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> type) throws IOException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        HessianInput hi = new HessianInput(bis);
+        return (T) hi.readObject(type);
+    }
+}
